41f60affde0837ee12b110737f0795c6ffde1f27
palomara <palomarangelll@hotmail.com>
2018-03-25 13:04:43 -0300

    operações matemáticas em R

---
+++ operacoesmatematicas.R
@@ -0,0 +1,24 @@
+#Operações básicas
+#Adição
+5 + 5
+
+#Subtração
+3 - 5
+
+#Divisão
+4 / 6
+
+#Multiplicação
+4 * 6
+
+#Exponenciação
+4 ^ 4
+
+#Resto de Divisão
+2 %% 2
+
+#Ordem de parenteses
+2 * (2 + 1)
+
+#Ordem Matemática
+1 + 2 * 2
