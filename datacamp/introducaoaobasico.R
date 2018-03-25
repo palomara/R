63ebfc9928ec5f693f7662ebfb3903559c45cd16
palomara <palomarangelll@hotmail.com>
2018-03-25 12:49:05 -0300

    básico

---
+++ datacamp/introducaoaobasico.R
@@ -0,0 +1,50 @@
+#Aritmética com R
+# Adição
+5 + 5 
+
+# Subtração
+5 - 5 
+
+# Multiplicação
+3 * 5
+
+# Divisão
+(5 + 5) / 2 
+
+# Armazena o valor 42 na variável x
+x <- 42
+
+# Exibe o valor da variável x
+x
+
+
+#Tipos de Dados básicos em R
+
+4.5 #número decimal, chamado de numerics
+4 #número inteiro; chamados de integers, mas também são numerics
+
+#valores booleanos, como por exemplo TRUE or FALSE são chamados de logical
+#textos, também conhecidos como strings, são chamados de characters
+
+# Altere o valor de my_numeric para 42
+my_numeric <- 42
+
+# Altere o valor de my_character para "universe"
+my_character <- "universe"
+
+# Altere o valor de my_logical para FALSE
+my_logical <- FALSE
+
+# Declare variáveis de diferentes tipos
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Verifique a classe de my_numeric
+class(my_numeric)
+
+# Verifique a classe da variável my_character
+class (my_character)
+
+# Verifique a classe de my_logical
+class (my_logical)
