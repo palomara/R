8021f5ce2590c365b84241e13211fb4372bf9911
palomara <palomarangelll@hotmail.com>
2019-04-25 00:53:55 -0300

    dir organization

---
+++ datacamp/vectors/arithmetic_vectors.R
@@ -0,0 +1,12 @@
+#Arithmetic vectors
+
+#Calculating total winnings 
+
+A_vector <- c(1, 2, 3)
+B_vector <- c(4, 5, 6)
+
+#Take the sum of A_vector and B_vector
+total_vector <- A_vector + B_vector
+
+#Print out total_vector
+print(total_vector)
---
+++ datacamp/vectors/naming_vectors_2.R
@@ -0,0 +1,12 @@
+#Poker winnings from Monday to Friday 
+poker_vector <- c (140, -50, 20, -120, 240)
+
+#Roullete winnings from Monday to Friday
+roullete_vector <- c (-24, -50, 100, -350, 10)
+
+#The variable days_vector
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+#Assign the names of the day to roullete_vector and poker_vector
+names (poker_vector) <- days_vector
+names (roullete_vector) <- days_vector
